d4156195c6ff6b7d4aed8f6c7c5ecbabdc4057c6
Gurshobit Brar <75352758+developer102-sp@users.noreply.github.com>
2022-05-01 16:56:37 +0530

    Code Commit
    Code Commit

---
+++ src/com/gurshobit/questionone/BalanceBrackets.java
@@ -0,0 +1,75 @@
+package com.gurshobit.questionone;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalanceBrackets {
+    public static Stack<Character> bracketStack = new Stack<>();
+
+    public static boolean isStackBalanced(String inputString){
+
+        if(inputString == null || inputString.toCharArray().length == 0 ){
+            System.out.println("String is empty");
+            return false;
+        }
+
+        if(inputString.contains("(") || inputString.contains(")") || inputString.contains("{") || inputString.contains("}") || inputString.contains("[") || inputString.contains("]")){
+            char inputStringArray[] = inputString.toCharArray();
+            for(int i = 0; i < inputStringArray.length; i++){
+                switch (inputStringArray[i]){
+                    case '(' :
+                    case '{' :
+                    case '[' :
+                        bracketStack.push(inputStringArray[i]);
+                        break;
+                    case ')' :
+                        if(!bracketStack.isEmpty()){
+                            if(bracketStack.peek() == '('){
+                                bracketStack.pop();
+                            }
+                        }
+                        break;
+                    case '}' :
+                        if(!bracketStack.isEmpty()){
+                            if(bracketStack.peek() == '{'){
+                                bracketStack.pop();
+                            }
+                        }
+                        break;
+                    case ']' :
+                        if(!bracketStack.isEmpty()){
+                            if(bracketStack.peek() == '['){
+                                bracketStack.pop();
+                            }
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid String");
+                }
+            }
+        } else {
+            System.out.println("Invalid String");
+            return false;
+        }
+
+        if(!bracketStack.isEmpty()){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scannerInput = new Scanner(System.in);
+        System.out.println("Enter Brackets String");
+        String inputStringValue = scannerInput.nextLine();
+
+        if(isStackBalanced(inputStringValue)){
+            System.out.println("The entered String has Balanced Brackets");
+
+        } else {
+            System.out.println("The entered String do not contain Balanced Brackets");
+        }
+    }
+
+}
---
+++ src/com/gurshobit/questiontwo/FindPairSum.java
@@ -0,0 +1,107 @@
+package com.gurshobit.questiontwo;
+
+import java.util.ArrayList;
+import java.util.Scanner;;
+
+public class FindPairSum {
+    Node treeRoot;
+    private ArrayList<Integer> listOne = new ArrayList<>();
+    private ArrayList<Integer> listTwo;
+
+    FindPairSum(){
+        treeRoot = null;
+    }
+
+    void insertTreeBranch(int data)
+    {
+        treeRoot = insertBranchNode(treeRoot, data);
+    }
+
+    Node insertBranchNode(Node root, int data)
+    {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data)
+            root.left = insertBranchNode(root.left, data);
+        else if (data > root.data)
+            root.right = insertBranchNode(root.right, data);
+
+        return root;
+    }
+
+    ArrayList<Integer> convertToList(Node treeNode , ArrayList<Integer> arrayList){
+        if(treeNode == null){
+            return arrayList;
+        }
+        convertToList(treeNode.left,arrayList);
+        arrayList.add(treeNode.data);
+        convertToList(treeNode.right,arrayList);
+        return arrayList;
+    }
+
+    boolean isPairOccurs(int sum){
+        listTwo = convertToList(treeRoot, listOne);
+        int startPointer = 0;
+        int endPointer = listTwo.size() - 1;
+        while(startPointer < endPointer){
+            int elementOne = listTwo.get(startPointer);
+            int elementTwo = listTwo.get(endPointer);
+
+            if( elementOne + elementTwo  == sum){
+                System.out.println("Pair ("+elementOne+","+elementTwo+") has sum of "+sum);
+                return true;
+            }
+
+            if (elementOne + elementTwo < sum) {
+                startPointer++;
+            }
+
+            if (elementOne + elementTwo > sum) {
+                endPointer --;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scannerInput = new Scanner(System.in);
+        FindPairSum findPairSum = new FindPairSum();
+
+        findPairSum.insertTreeBranch(40);
+        findPairSum.insertTreeBranch(20);
+        findPairSum.insertTreeBranch(10);
+        findPairSum.insertTreeBranch(30);
+        findPairSum.insertTreeBranch(60);
+        findPairSum.insertTreeBranch(50);
+        findPairSum.insertTreeBranch(70);
+
+        int testCases = 0;
+        do{
+            System.out.println("Enter number of Test Cases");
+            testCases = Integer.parseInt(scannerInput.nextLine());
+        } while(testCases == 0);
+        int testCasesCounter = testCases;
+        int testCasesArray[] = new int[testCasesCounter];
+
+        for(int j = 0; j < testCasesCounter; j++){
+            System.out.println("Enter Test Case ("+(j+1)+"):");
+            testCasesArray[j] = Integer.parseInt(scannerInput.nextLine());
+        }
+
+        for(int k = 0; k < testCasesArray.length; k++) {
+            System.out.println("Test Case ("+(k+1)+"): SUM("+testCasesArray[k]+")");
+            if(findPairSum.isPairOccurs(testCasesArray[k])){
+                System.out.println("Pair Found Successfully");
+            } else {
+                System.out.println("Pair Not Found having sum " + testCasesArray[k]);
+            }
+            System.out.println();
+        }
+    }
+
+}
